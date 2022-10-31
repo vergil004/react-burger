01a091f8847514edd35bbec87efac7f82c928f27
vergil004 <vergil004@gmail.com>
2022-10-31 09:59:32 +0500

    step2: первый коммит

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import {AppHeader} from "./components/app-header/app-header";
 import {BurgerIngredients} from "./components/burger-ingredients/burger-ingredients";
 import {BurgerConstructor} from "./components/burger-constructor/burger-constructor";
 import './App.css';
-import ingredientsList from './utils/data.json'
 
 export function App (){
 
-    const [ingredients, setIngredients] =useState(ingredientsList)
+    const [state, setState] =useState({
+        ingredients: [],
+        isLoading: false,
+        hasError: false
+    })
+    const ingredientsDataUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
+    const fetchData = async () => {
+        await fetch(ingredientsDataUrl)
+        .then(response => response.json())
+        .then(data => setState({...state, ingredients: data.data, isLoading: false}))
+        .catch(setState({...state, isLoading: false, hasError: true}))
+    }
+
+    React.useEffect(() => {
+        setState({...state, isLoading: true})
+        fetchData()
+    },[])
 
      return (
         <div className="App">
           <AppHeader/>
           <main className='pt-10 main'>
-              <BurgerIngredients ingredients={ingredients}/>
-              <BurgerConstructor ingredients={ingredients}/>
+              {state.ingredients.length > 0 &&<BurgerIngredients ingredients={state.ingredients}/>}
+              {state.ingredients.length > 0 &&<BurgerConstructor ingredients={state.ingredients}/>}
           </main>
         </div>
       )
