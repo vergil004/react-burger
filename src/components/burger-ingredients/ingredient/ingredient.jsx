38f709f57657b6678a58153ef0e7a54e85c0f8b9
vergil004 <vergil004@gmail.com>
2022-11-03 15:16:18 +0500

    master: добавил eslint

--- src/components/burger-ingredients/ingredient/ingredient.jsx
+++ src/components/burger-ingredients/ingredient/ingredient.jsx
@@ -1,44 +1,60 @@
-import React, {useState} from "react";
-import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
-import {Modal} from '../../modal/modal'
-import { IngredientDetails } from '../../ingredient-details/ingredient-details'
-import ingredientStyle from './ingredient.module.css'
-import {ingredientPropTypes} from '../../../utils/types'
+import React, { useState } from "react";
+import {
+  CurrencyIcon,
+  Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Modal } from "../../modal/modal";
+import { IngredientDetails } from "../../ingredient-details/ingredient-details";
+import ingredientStyle from "./ingredient.module.css";
+import { ingredientPropTypes } from "../../../utils/types";
 
-export function Ingredient({ingredient}){
-    const [count, setCount] = useState(1)
-    const [showModal, setShowModal] = useState(false)
+export function Ingredient({ ingredient }) {
+  const count = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-        <>
-            {showModal &&
-                <Modal title='Детали ингредиента' closeModal={() => setShowModal(false)}>
-                    <IngredientDetails ingredient={ingredient}/>
-                </Modal>
-            }
-            <div className={`${ingredientStyle.ingredient} pr-4 pl-4`} onClick={() => setShowModal(true)}>
-                {count > 0 &&
-                    <div className={ingredientStyle.ingredient__count} >
-                        <Counter count={count}/>
-                    </div>
-                }
-                <img className={ingredientStyle.ingredient__image} src={ingredient.image_large}
-                     alt={ingredient.name} />
-                <div className={`${ingredientStyle.ingredient__price} pt-1 pb-1`}>
-                    <span className="text text_type_digits-default pr-2">{ingredient.price}</span>
-                    <CurrencyIcon type={"primary"}/>
-                </div>
-                <div className={`${ingredientStyle.ingredient__name} text text_type_main-default`}>
-                    {ingredient.name}</div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      {showModal && (
+        <Modal
+          title="Детали ингредиента"
+          closeModal={() => setShowModal(false)}
+        >
+          <IngredientDetails ingredient={ingredient} />
+        </Modal>
+      )}
+      <div
+        className={`${ingredientStyle.ingredient} pr-4 pl-4`}
+        onClick={() => setShowModal(true)}
+      >
+        {count > 0 && (
+          <div className={ingredientStyle.ingredient__count}>
+            <Counter count={count} />
+          </div>
+        )}
+        <img
+          className={ingredientStyle.ingredient__image}
+          src={ingredient.image_large}
+          alt={ingredient.name}
+        />
+        <div className={`${ingredientStyle.ingredient__price} pt-1 pb-1`}>
+          <span className="text text_type_digits-default pr-2">
+            {ingredient.price}
+          </span>
+          <CurrencyIcon type={"primary"} />
+        </div>
+        <div
+          className={`${ingredientStyle.ingredient__name} text text_type_main-default`}
+        >
+          {ingredient.name}
+        </div>
+      </div>
+    </>
+  );
 }
 
 Ingredient.defaultProps = {
-    ingredient: {},
-}
+  ingredient: {},
+};
 Ingredient.propTypes = {
-    ingredient: ingredientPropTypes
-}
-
+  ingredient: ingredientPropTypes,
+};
--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,36 +1,44 @@
 import React from "react";
-import PropTypes from "prop-types";
-import detailsStyles from './ingredient-details.module.css'
-import {ingredientPropTypes} from '../../utils/types'
+import detailsStyles from "./ingredient-details.module.css";
+import { ingredientPropTypes } from "../../utils/types";
 
-
-export function IngredientDetails ({ingredient}){
-    return(
-        <div className={detailsStyles.details}>
-            <img src={ingredient.image_large}/>
-            <h2 className={`${detailsStyles.details__title}pt-4 pb-8 text text_type_main-medium`}>{ingredient.name}</h2>
-            <ul className={detailsStyles.details__grid}>
-                <li className={detailsStyles.details__item}>
-                    <div className='text_type_main-default'>Калории,ккал</div>
-                    <div className='text_type_main-default pt-4'>{ingredient.calories}</div>
-                </li>
-                <li className={detailsStyles.details__item}>
-                    <div className='text_type_main-default'>Белки, г</div>
-                    <div className='text_type_main-default pt-4'>{ingredient.proteins}</div>
-                </li>
-                <li className={detailsStyles.details__item}>
-                    <div className='text_type_main-default'>Жиры, г</div>
-                    <div className='text_type_main-default pt-4'>{ingredient.fat}</div>
-                </li>
-                <li className={detailsStyles.details__item}>
-                    <div className='text_type_main-default'>Углеводы, г</div>
-                    <div className='text_type_main-default pt-4'>{ingredient.carbohydrates}</div>
-                </li>
-            </ul>
-        </div>
-    )
+export function IngredientDetails({ ingredient }) {
+  return (
+    <div className={detailsStyles.details}>
+      <img src={ingredient.image_large} />
+      <h2
+        className={`${detailsStyles.details__title}pt-4 pb-8 text text_type_main-medium`}
+      >
+        {ingredient.name}
+      </h2>
+      <ul className={detailsStyles.details__grid}>
+        <li className={detailsStyles.details__item}>
+          <div className="text_type_main-default">Калории,ккал</div>
+          <div className="text_type_main-default pt-4">
+            {ingredient.calories}
+          </div>
+        </li>
+        <li className={detailsStyles.details__item}>
+          <div className="text_type_main-default">Белки, г</div>
+          <div className="text_type_main-default pt-4">
+            {ingredient.proteins}
+          </div>
+        </li>
+        <li className={detailsStyles.details__item}>
+          <div className="text_type_main-default">Жиры, г</div>
+          <div className="text_type_main-default pt-4">{ingredient.fat}</div>
+        </li>
+        <li className={detailsStyles.details__item}>
+          <div className="text_type_main-default">Углеводы, г</div>
+          <div className="text_type_main-default pt-4">
+            {ingredient.carbohydrates}
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 IngredientDetails.propTypes = {
-    ingredient: ingredientPropTypes.isRequired
-}
+  ingredient: ingredientPropTypes.isRequired,
+};
