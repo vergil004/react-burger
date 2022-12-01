38cdda66cdb3b5b8dc127a90b6a66e4bc6ce3fa8
vergil004 <vergil004@gmail.com>
2022-12-01 16:05:36 +0500

    master: fix cookie

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,5 +1,8 @@
 export function setCookie(name, value, props) {
-  props = props || {};
+  props = props || {
+    path: "/",
+    ...props,
+  };
   let exp = props.expires;
   if (typeof exp == "number" && exp) {
     const d = new Date();
